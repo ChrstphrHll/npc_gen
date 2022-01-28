adbdeeccb73940380cc385ff52bd5dfb5b8fbc08
Christopher Hall II <christopher.s.hall@duke.edu>
2022-01-28 12:48:42 -0500

    added functional function to fill out rosters of organizations

---
+++ organizational_utilities/assign_organizations.py
@@ -0,0 +1,24 @@
+
+class Organization():
+    def __init__(self, name, size, validators):
+        self.name = name
+        self.size = size
+        self.validators = validators
+        self.members = []
+        self.leaders = []
+        self.supervisors = []
+
+    def fill_out_roster(self, students):
+        roster = []
+
+        while len(roster) < self.size:
+            candidate = students.pop()
+            meets_requirements = True
+            
+            for validator in self.validators:
+                if not validator(candidate):
+                    meets_requirements = False
+                    break
+            
+            if meets_requirements:
+                roster.append(candidate)
